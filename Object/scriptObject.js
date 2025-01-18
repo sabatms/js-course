66f9ab3bb8cc8dea11a7080855532ed0a260d8cd
Saba Tahmasebinezhad <saba.tms15@gmail.com>
2025-01-17 23:15:32 -0800

    add scriptObject.js file

---
+++ Object/scriptObject.js
@@ -0,0 +1,21 @@
+const key1 = "tennis";
+
+const data = {
+  name: "saba",
+  lastname: "tahmasebi",
+  age: 26,
+  country: "IR",
+  skils: ["html", "css", "js", "react", "next.js"],
+  6: "yes",
+  [key1]: "my favaorut sport",
+};
+console.log(data);
+
+//const key = "skils";
+// console.log(data.name);
+// console.log(data[key]);
+// console.log(data[6]);
+// console.log(data);
+
+// افزودن دیتا به object:
+// باید اون متغیر رو داخل[]  که میخوایم
