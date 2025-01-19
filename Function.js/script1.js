78eb56d3f5af000a6929aa761ff0ac8a27994136
Saba Tahmasebinezhad <saba.tms15@gmail.com>
2025-01-19 20:41:10 +0330

    add script.js file

---
+++ Function.js/script1.js
@@ -0,0 +1,24 @@
+// function = تابع
+// block of code => dont repeat again code
+
+function dublicate(number){
+    const result = number * 8;
+    console.log(result)
+}
+
+dublicate(3);
+
+function sum( num1 , num2 ){
+    const result1 = num1 + num2;
+    console.log(result1);
+}
+sum(4,5);
+
+
+// return :
+
+function sum1( num1 , num2){
+    const result2 = num1 * num2;
+    return result2
+}
+console.log(sum1(3,4));
