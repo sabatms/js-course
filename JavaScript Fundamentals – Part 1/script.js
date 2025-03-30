92df53afc56931100e27dc76414398402d8d1169
Saba Tahmasebinezhad <saba.tms15@gmail.com>
2025-03-30 16:26:21 +0330

    add script.js file

---
+++ JavaScript Fundamentals – Part 1/script.js
@@ -0,0 +1,104 @@
+console.log(40 + 8 + 23 - 10);
+
+// Values and Variables
+let FirstName = "saba";
+console.log(FirstName);
+
+// Variable name conventions:
+console.log(25);
+let first1 = "fati";
+
+console.log(first1);
+console.log(first1);
+console.log(first1);
+
+//Variable name conventions:
+let jonas_matilda = 'JM';
+let $Function = 26; // its not true : $function is technically allowed since $ differentiates it from a reserved keyword, but using it can reduce code readability and cause confusion.
+
+let person = "fatemeh";
+const PI = 3.14;
+
+let myFirstJob = "front-end";
+let myCurrentJob = "Coder";
+
+let job1 = "programer";
+let job2 = "AI Developer";
+
+console.log(myFirstJob);
+
+let country = 'Iran';
+let continent = 'Asia';
+let population = 85;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types :
+
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof JavascriptIsFun);
+console.log(typeof 26);
+console.log(typeof 'Fatemeh');
+
+JavascriptIsFun = "Yes!";
+console.log(typeof JavascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1998;
+console.log(typeof year);
+
+console.log(typeof null);
+
+// Data type challenge :
+
+// 1) Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+// 2)Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false;
+let language;
+
+let population1 = 85000000;
+let country1 = 'Iran';
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population1);
+console.log(typeof country1);
+
+// let, const and var :
+
+let age = 26;
+age = 27;
+console.log(age);
+
+const birthday2 = 1999
+//birthday2 = 1998
+//console.log(birthday2);
+
+var city = "Tehran";
+var city = "mashhad";
+
+console.log(city);
+
+// exp for let-var-const;
+
+//Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+//Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+//Try to change one of the changed variables now, and observe what happens.
+
+let language2 = "Persian";
+
+const country2 = "Iran";
+const continent2 = "Asia";
+let isIsland2 = false;
+isIsland2 = true;
+console.log(isIsland2);
+
